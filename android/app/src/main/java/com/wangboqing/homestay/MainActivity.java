3168ca5abc9ec88ed50ef2373ec334a4d8899dbb
Wang-1103-boqing <1320944069@qq.com>
2026-05-05 23:08:52 +0100

    feat(capacitor): add Android platform with Aliyun Gradle mirror for China users

---
+++ android/app/src/main/java/com/wangboqing/homestay/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wangboqing.homestay;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wangboqing.homestay',
+  appName: '民宿管家',
+  webDir: 'dist'
+};
+
+export default config;
